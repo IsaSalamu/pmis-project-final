7a2771c721839461b471d96b372af17302b63ee5
IsaSalamu <bsc-04-17@cc.ac.mw>
2022-01-08 09:10:26 +0200

    css background color transformation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,21 +5,21 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-              <nav className="navbar navbar-light bg-primary">
+              <nav className="navbar navbar-light bg_color">
                   <div className="container-fluid">
                     <a className="navbar-brand" style={{color:"whitesmoke"}}>Poultry Management Information System</a>
 
                     <div className="d-flex">
                           <OverlayTrigger placement="left-start" overlay={<Tooltip id="tooltip-disabled" >Admin</Tooltip>}>
                             <span className="d-inline-block">
-                              <span  className="btn btn-primary position-relative" style={{marginRight:"10px"}}>
+                              <span  className="btn bg_color position-relative" style={{marginRight:"10px"}}>
                                 <FaIcons.FaUserCircle/>
 
                               </span>
                             </span>
                           </OverlayTrigger>
                         
-                        <span  className="btn btn-primary position-relative" style={{marginRight:"20px"}}>
+                        <span  className="btn bg_color position-relative" style={{marginRight:"20px"}}>
                             <FaIcons.FaEnvelope/>
 
                             <span style={{marginTop:"10px"}} className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">
